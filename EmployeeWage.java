0ae873f0596d0a2fa0cab73a114e628c3bb62c61
Jayakansal <21jayajindal@gmail.com>
2020-11-28 12:47:05 +0530

    Added: Employee_Attendance_logic

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,21 @@
+import java.util.Random;
 
 public class EmployeeWage {
 
       public static void  main (String[] args)
 {
   System.out.println("Welcome to Employee Wage Computation Using Java");
-}
-}
 
+
+                Random random=new Random();
+                        int randomNum=random.nextInt(2);
+
+if (randomNum==1) {
+        System.out.println("Employee is Present");
+}
+ else {
+        System.out.println("Employee is Absent");
+        }
+        }
+
+}
