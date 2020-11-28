e324387314b28b5303f2072e45327456f6a935ff
Jayakansal <21jayajindal@gmail.com>
2020-11-28 13:07:24 +0530

    Added: PartTime_FullTime_Wage_logic

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,27 +1,37 @@
 import java.util.Random;
 
-public class EmployeeWage {
+public class PartTimeORFullTime {
 
       public static void  main (String[] args)
 {
   System.out.println("Welcome to Employee Wage Computation Using Java");
 
 
+                int PartTime=1;
+                int FullTime=2;
+                int EmpRatePerHr=20;
+                int EmpHur=0;
+
                 Random random=new Random();
-                        int randomNum=random.nextInt(2);
+                int randomNum=random.nextInt(3);
+                if (randomNum==1)
+                {
+                 EmpHur=4;
+                }
+                else if (randomNum==2) {
+                 EmpHur=8;
+                }
+                else {
+                         EmpHur=0;
+                }
 
-if (randomNum==1) {
-System.out.println("Employee is Present");
-int EmpHur=8;
-int ratePerHur=20;
-System.out.println("Salary:(int EmpHur* int ratePerHur)}");
-
-} else {
-System.out.println("Employee is Absent");
-System.out.println("Salary:+0");
-}
-}
-}
+                System.out.println("Employee Hours"+ EmpHur);
+                System.out.println("Salary:"+( EmpHur*EmpRatePerHr));
 
 
-}
+                }
+
+
+        }
+
+
