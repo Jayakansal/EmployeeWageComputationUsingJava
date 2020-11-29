a96bcf8d9e2c8d27b091011b82c7416bc6d1e778
Jayakansal <21jayajindal@gmail.com>
2020-11-29 16:34:48 +0530

    Added: Emp_Wageof_mutilpleCompany_Inteface_logic

---
+++ IComputeEmployeeWage.java
@@ -0,0 +1,11 @@
+public interface IComputeEmpWage {
+
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+
+	public void computeEmpWage();
+
+	public int computeEmpWage(CompanyEmpWage companyEmpWage);
+
+	public int getTotalWage(String company);
+
+}
