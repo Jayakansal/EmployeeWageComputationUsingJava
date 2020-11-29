844a2aa8a2210fc238bf603c284dcf7b7c91b55a
Jayakansal <21jayajindal@gmail.com>
2020-11-29 21:02:53 +0530

    Refactor: Employee_Wageof_mutilpleCompany_list_logic

---
+++ employeeWageBuilderList.java
@@ -0,0 +1,74 @@
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class EmployeeWageBuilderArray implements IComputeEmpWage {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+
+	public int numOfCompany = 0;
+
+	private LinkedList<CompanyEmpWage> companyEmpWageList;
+
+
+
+
+	public EmployeeWageBuilderArray() {
+		companyEmpWageList = new LinkedList<CompanyEmpWage>();
+
+	}
+
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+
+		companyEmpWageList.add(companyEmpWage);
+	}
+
+	public void computeEmpWage() {
+		for (int i = 0; i < companyEmpWageList.size(); i++) {
+			CompanyEmpWage company = companyEmpWageList.get(i);
+			company.setTotalEmpWage(this.computeEmpWage(company));
+			System.out.println(company);
+
+		}
+	}
+
+	public int computeEmpWage(CompanyEmpWage companyEmpWage) {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		Random random = new Random();
+		while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
+			totalWorkingDays++;
+
+			int empCheck = random.nextInt(3);
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("day: " + totalWorkingDays + "EmpHrs: " + empHrs);
+		}
+
+		return totalEmpHrs * companyEmpWage.empRatePerHour;
+	}
+
+	public static void main(String[] args) {
+		EmployeeWageBuilderArray empWageBuilder = new EmployeeWageBuilderArray();
+		empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+		empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+		empWageBuilder.computeEmpWage();
+	}
+
+	@Override
+	public int getTotalWage(String company) {
+		return companyEmpWageMap.get(company).totalEmpWage;
+
+	}
+
+}
